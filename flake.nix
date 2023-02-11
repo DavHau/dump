c8c32d8b116f01a79ee812d70a72806704a3604e
DavHau <hsngrmpf+github@gmail.com>
2023-02-11 20:38:53 +0700

    add odoo python package

--- flake.nix
+++ flake.nix
@@ -3,24 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgsPython.url = "github:davhau/nixpkgs/davhau-fetchPythonRequirements";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
+    drv-parts.url = "github:DavHau/drv-parts";
+    drv-parts.inputs.nixpkgs.follows = "nixpkgs";
+    drv-parts.inputs.flake-parts.follows = "flake-parts";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
 
       systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
         "x86_64-linux"
       ];
 
       imports = [
-        ./nix/modules/flake-parts/modules.nix
+        ./nix/modules/flake-parts/all-modules.nix
       ];
     };
 }
